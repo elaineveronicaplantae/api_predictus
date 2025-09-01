7607f1431a64c4b3136d8cacbd2504f75506205e
elaineveronicaplantae <elaine.veronica@plantaeagrocredito.com.br>
2025-09-01 09:28:41 -0300

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,6 @@
+﻿from app.auth.database import engine, Base
+from app.auth.models import User
+
+print("INFO:     Criando tabelas do banco de dados...")
+Base.metadata.create_all(bind=engine)
+print("INFO:     Tabelas do banco de dados criadas com sucesso.")
